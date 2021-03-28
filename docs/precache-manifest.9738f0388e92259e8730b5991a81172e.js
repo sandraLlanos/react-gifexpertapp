25960dc0832340bd7d8b03127dabfbd8288ac5ca
sandra <sandra.llanos.alvarez@gmail.com>
2021-03-28 17:31:11 -0500

    docs added

---
+++ docs/precache-manifest.9738f0388e92259e8730b5991a81172e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1a54a547da297e50de56502f1e6a95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed244dd4677e7e4b7b12",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed244dd4677e7e4b7b12",
+    "url": "/static/js/main.536a9a35.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
